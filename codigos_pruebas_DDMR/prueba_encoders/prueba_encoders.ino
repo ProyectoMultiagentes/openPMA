0ba8f12a1ad4ac75c2cc99e163b4a015a751b7a5
cescobar <carlos.escobarv@sansano.usm.cl>
2022-01-04 21:08:55 -0300

    Se agregan códigos actualizados
    Códigos actualizados para realizar pruebas a los sensores y actuadores del robot móvil diferencia (DDMR)

---
+++ codigos_pruebas_DDMR/prueba_encoders/prueba_encoders.ino
@@ -0,0 +1,22 @@
+/*
+Este programa permite la lectura de los pulsos de salida de los encoders
+dispuestos en cada una de las ruedas del RMD.
+-Requisitos: Libreria Encoder.h <http://www.pjrc.com/teensy/td_libs_Encoder.html>
+-Cofiguracion: Al visualizar por consola los valores entregados por los encoders, verificar si el incremento/disminucion observado coincide 
+                con el sentido de giro de las ruedas del Robot (adelante/atras).
+*/
+#include <Encoder.h>
+
+Encoder encoder_der(5, 23);          //Corresponde a la estructura "encoder_der(PIN1, PIN2)" que lee los pulsos desde del encoder de la rueda derecha (invertir orden de ser necesario)
+Encoder encoder_izq(18, 19);         //Corresponde a la estructura "encoder_izq(PIN1, PIN2)" que lee los pulsos desde del encoder de la rueda izquierda (invertir orden de ser necesario)
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {                       //Se imprime en pantalla las cuentas de los encoder utilizando la funcion encoder.read()
+  Serial.print(encoder_izq.read()); 
+  Serial.print(" - ");
+  Serial.print(encoder_der.read());
+  Serial.println();
+}
